8dc61da31c407929fd1b1679c17be74e918da35b
Zaheir Razak <zaheir786@hotmail.com>
2023-09-07 14:21:09 -0400

    button has correct intial color test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import { logRoles } from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Learn React/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+  //const {container} = render(<App />);
+  render(<App />)
+  //logRoles(container);
+
+  //finding an element with the role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name : 'Change to blue' });
+
+  expect(colorButton).toHaveStyle({backgroundColor : 'red'});
 });
+test('button has correct initial text', () => {
+  
+});
+
+test('button has clicked turns blue', () => {
+  
+});
+
+
+
